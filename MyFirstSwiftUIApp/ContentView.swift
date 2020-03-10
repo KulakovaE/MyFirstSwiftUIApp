39377261b13a3076af7aa8c42277e223cd94c5f0
Elena Kulakova <egeorgievska89@gmail.com>
2020-03-10 12:23:53 +0100

    Initial Commit

---
+++ MyFirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftUIApp
+//
+//  Created by Elena Kulakova on 2020-03-10.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
