ef7106f5d4006b118f6fb2a56483699b4b27663e
Elena Kulakova <egeorgievska89@gmail.com>
2020-03-10 13:28:01 +0100

    Add files from Apple project.

--- MyFirstSwiftUIApp/ContentView.swift
+++ MyFirstSwiftUIApp/ContentView.swift
@@ -9,23 +9,21 @@
 import SwiftUI
 import MapKit
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         VStack {
             MapView()
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
+            
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
-                    .font(.largeTitle)
-                    .fontWeight(.thin)
-                    .multilineTextAlignment(.center)
-                    .lineLimit(0)
-                    .padding(0.0)
+                    .font(.title)
+                    
                 HStack {
                     Text("JoshuaTree National Park")
                         .font(.subheadline)
@@ -42,6 +40,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail()
     }
 }
